adc2473728b361fcbb15c31ed4deb58216aa0a37
Stephen <sheldon2521@gmail.com>
2019-03-05 17:59:10 -0500

    Renamed files

--- InsertionSort.c
+++ InsertionSort.c
@@ -1,15 +1,18 @@
-#include <stdio.h> 
-
 /*
 ** Author: Stephen Sheldon 3/3/2019
+** A simple insertion sort
+** Runtime: O(n^2)
 */
 
-void insertion_sort(int *arr, int len) {
-	int key, j;
-	for (int i = 1; i < len; i++) {
+void insertion_sort(int *arr, int len) 
+{
+	int key, i, j;
+	for (i = 1; i < len; i++) 
+	{
 		key = arr[i];
 		j = i - 1;
-		while (j >= 0 && arr[j] > key) {
+		while (j >= 0 && arr[j] > key) 
+		{
 			arr[j+1] = arr[j];
 			j--;
 		}
@@ -17,16 +20,3 @@ void insertion_sort(int *arr, int len) {
 	}
 }
 
-int main() {
-	
-	int arr[] = {4, 5, 4, 3, 2, 9, 44, 2, 3, 5, 5, 5, 3, 0};
-	int len = 14;
-	
-	insertion_sort(arr, len);
-	
-	for (int i = 0; i < len; i++) {
-		printf("%d ", arr[i]);
-	}
-	
-	exit(0);
-}
--- SelectionSort.c
+++ SelectionSort.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 /*
 ** Author: Stephen Sheldon 3/5/2019
 **
@@ -26,47 +24,5 @@ void selection_sort(int *arr, int len)
 	}
 }
 
-void printArray(int *arr, int len) {
-	int i;
-	
-	for (i = 0; i < len; i++) 
-		printf("%d ", arr[i]);
-	
-	printf("\n");
-}
 
-int main() {
-	
-	// File Format: First line is number of integers - n
-	//              Followed by integers on n lines 	
-	FILE *file = fopen ("sortintegers.txt", "r");
-	int i, len;
-		
-	if (file == NULL) 
-	{
-		printf("Cannot open file\n");
-		exit(0);
-	}
-		
-	while(!feof (file)) 
-	{
-		fscanf(file, "%d", &len);
-		
-		int arr[len];
-		
-		for(i = 0; i < len; i++)
-			fscanf(file, "%d", &arr[i]);
-		
-		printf("Array before sorting is: ");
-		printArray(arr, len);
-		
-		selection_sort(arr, len);
-		
-		printf("Array after sorting is: ");
-		printArray(arr, len);
-	}
-	
-	fclose(file);
 
-	exit(0);
-}
