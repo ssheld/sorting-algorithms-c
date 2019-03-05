c3865961c42f96ed7776792fee8d159d0539b9c3
Stephen <sheldon2521@gmail.com>
2019-03-05 18:08:52 -0500

    Adding renamed files

---
+++ selectionSort.c
@@ -0,0 +1,28 @@
+/*
+** Author: Stephen Sheldon 3/5/2019
+**
+** A simple Selection Sort for integers
+** Runtime: O(n^2)
+*/
+
+void selection_sort(int *arr, int len) 
+{
+	int i, j, min, temp;
+	
+	for (i = 0; i < len; i++) 
+	{
+		min = i;
+		// Find min in current array relative to current min
+		for (j = i+1; j < len; j++) 
+			if (arr[j] < arr[min]) 
+				min = j;
+		
+		// Swap here
+		temp = arr[i];
+		arr[i] = arr[min];
+		arr[min] = temp;
+	}
+}
+
+
+
